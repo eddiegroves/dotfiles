c4d18022d411faf368b2d82b8c526341742bf2c4
Eddie Groves <eddiegroves@gmail.com>
2012-10-05 15:06:34 +1000

    Adding support for ConEmu and configs that exist in my 'bin' directory

--- makesymlinks.bat
+++ makesymlinks.bat
@@ -1,5 +1,4 @@
-:: Manually map each config file for now
+:: Edit the ps1 file to add new dotfiles files
 @powershell -NoProfile ./makesymlinks.ps1
-
 
 :: mklink /d %HOMEDRIVE%\.vim %HOMEDRIVE%\dotfiles\vim
--- makesymlinks.ps1
+++ makesymlinks.ps1
@@ -1,5 +1,23 @@
-﻿'_vimrc', '.gitconfig', '.gitignore_global' | % {
+﻿# Creatse Windows symlinks for each dotfile manually specified
+$home_dir = '_vimrc', '.gitconfig', '.gitignore_global'
+$home_dir | % {
     if (-not (Test-Path "$HOME$_")) {
         & cmd /c "mklink $HOME$_ $($HOME)dotfiles\$($_.TrimStart('.', '_'))"
+    } else {
+        "Skipping $_ as it already exists at $HOME$_"
     }
+}
+
+$bin_dir = @{ 'ConEmu.xml' = 'ConEmu' }
+if (Test-Path variable:global:bin) {
+    $bin_dir.Keys | % { 
+        $path = "$bin\$($bin_dir.$_)\$_"
+        if (-not (Test-Path $path)) {
+            & cmd /c "mklink $path $($HOME)dotfiles\$_"
+        } else {
+            "Skipping $_ as it already exists at $path"
+        }
+    }
+} else {
+    "`$bin directory variable not set, where is it?"
 }
