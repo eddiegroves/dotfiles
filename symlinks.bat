41b766275418d9dde6b166029451a679e61c8894
Eddie Groves <eddiegroves@gmail.com>
2013-06-14 13:40:21 +1000

    Adding symlink for Powershell profile scripts

--- symlinks.bat
+++ symlinks.bat
@@ -1,2 +1,3 @@
 if not exist _vimrc mklink _vimrc dotfiles\vimrc 
 if not exist .gitignore_global mklink .gitignore_global dotfiles\gitignore_global 
+if not exist .\Documents\WindowsPowerShell mklink /D .\Documents\WindowsPowerShell ..\dotfiles\ps
