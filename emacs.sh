62cf08607b6330f8c4f58bf7118edcee1b9e16f8
Eddie Groves <git@eddiegroves.com>
2023-01-19 10:08:01 +1100

    Add note about Emacs in Spotlight
    Related: https://github.com/d12frosted/homebrew-emacs-plus/pull/517

--- emacs.sh
+++ emacs.sh
@@ -20,3 +20,4 @@ export PATH=$HOMEBREW_PREFIX:$PATH
 
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@29 --verbose --with-native-comp --with-xwidgets --with-poll --with-no-frame-refocus --with-nobu417-big-sur-icon
+echo "For Emacs to show up in Spotlight, see https://apple.stackexchange.com/a/32495"
