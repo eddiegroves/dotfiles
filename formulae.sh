5976f644132a0e0d63cb422f64d22adb3aecd0c4
Eddie Groves <git@eddiegroves.com>
2023-02-02 09:44:23 +1100

    Install kubectl

--- formulae.sh
+++ formulae.sh
@@ -40,6 +40,7 @@ brew install --formulae --quiet gojq
 brew install --formulae --quiet hledger
 brew install --formulae --quiet hunspell
 brew install --formulae --quiet kotlin-language-server
+brew install --formulae --quiet kubernetes-cli
 brew install --formulae --quiet libvterm
 brew install --formulae --quiet lsd
 brew install --formulae --quiet node@19
