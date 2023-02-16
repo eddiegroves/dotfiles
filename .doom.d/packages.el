7b5e065a6802ef59768d630d21dc1f1f549a9ad3
Eddie Groves <git@eddiegroves.com>
2023-02-17 05:39:50 +1100

    Doom Emacs: Bump consult-lsp package

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -7,3 +7,7 @@
 ;; https://github.com/merrickluo/lsp-tailwindcss
 (package! lsp-tailwindcss
   :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
+
+;; Fetch a newer version of consult-lsp which has some bugfixes
+(package! consult-lsp
+  :pin "771cfad7c383d9dbbb5559a9439cb4de127d2fe6")
