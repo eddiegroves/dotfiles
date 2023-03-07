32f98664367efd4cc80bc40708e1b62db9e2f89b
Eddie Groves <git@eddiegroves.com>
2023-03-08 05:21:33 +1100

    Install wrangler

--- npm.sh
+++ npm.sh
@@ -6,4 +6,5 @@ npm install --global pyright
 npm install --global typescript
 npm install --global typescript-language-server
 npm install --global vscode-langservers-extracted
+npm install --global wrangler
 npm install --global yaml-language-server
