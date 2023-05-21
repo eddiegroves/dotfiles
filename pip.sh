e9870d2fd5e070705cde09e13aac7f23bce3c060
Eddie Groves <git@eddiegroves.com>
2023-05-22 08:44:01 +1000

    Add global common libs for python

--- pip.sh
+++ pip.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pip install black[d]
+
+# Global common libraries
+# Packages I use in lots of places for simple scripts
+pip install orgparse
+pip install jira
+pip install pandoc==2.4b0
