71693b32d32d3d37f86bdbf3cc33ef1662585d9d
Eddie Groves <git@eddiegroves.com>
2023-05-22 10:57:03 +1000

    Consult LSP fix is now in mainline

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -8,12 +8,6 @@
 (package! lsp-tailwindcss
   :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 
-;; Fetch a newer version of consult-lsp which has some bugfixes
-(package! consult-lsp
-  :pin "771cfad7c383d9dbbb5559a9439cb4de127d2fe6")
-
 (package! flycheck-hledger)
-
 (package! denote)
-
 (package! csv-mode)
