27822cd6692d22caf38d232369d9a22c9ed03204
Eddie Groves <git@eddiegroves.com>
2023-10-05 06:14:49 +1100

    Add osxphotos CLI

--- pip.sh
+++ pip.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pip install black[d]
+pip install osxphotos
 
 # Global common libraries
 # Packages I use in lots of places for simple scripts
 pip install orgparse
 pip install jira
+pip install prettytable
 pip install pandoc==2.4b0
